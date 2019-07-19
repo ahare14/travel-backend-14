1e1359564158a3e83e617218db2a5cb3b93522c4
ahare14 <alexanderhare14@gmail.com>
2019-07-19 13:53:16 -0600

    controllers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,45 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :update, :destroy]
+
+  def index
+    @trips = Trip.all
+
+    render json: @trips
+  end
+
+  def show
+    render json: @trip
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      render json: @trip, status: :created, location: @user
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @trip.update(trip_params)
+      render json: @property
+    else
+      render json: @property.errors, status: : unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+  end
+
+  private
+
+  def set_trip
+    @trip = trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:user_id, :title)
+  end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  before_action :set_example, only: [:show, :update, :destroy]
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /examples/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_example
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:username, :password, :name, :email)
+    end
 end
