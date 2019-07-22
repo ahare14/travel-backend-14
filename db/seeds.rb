36e8d2a28a2ed69cd006ec39b4f661aac29f90fb
ahare14 <alexanderhare14@gmail.com>
2019-07-22 12:02:44 -0600

    added additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: 'example@aol.com', name: 'John Smith', username:'john_smith_1', password:'P@ssw0rd')
+
+Trip.create(user_id: 1, title: 'Denver Getaway')
+
+Picture.create(trip_id: 1, img_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Tremont_Mtn_Golden_Gate_State_Park.jpg/284px-Tremont_Mtn_Golden_Gate_State_Park.jpg', description: 'Beautiful mountain weekend getaway', latitude: 105.4198, longitude: 39.8414)
+
+
+
