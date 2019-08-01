0b7bc7bf29876e4a6ede8374c49a96c417390c99
ahare14 <alexanderhare14@gmail.com>
2019-07-31 18:46:48 -0600

    fixed something

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -55,5 +55,5 @@ class TripsController < ApplicationController
   #       ] 
   #     ]
   #   )
-  end 
+  
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,21 +1,21 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :pictures, dependent: :destroy
-  accepts_nested_attributes_for :covers, allow_destroy: true
+  # accepts_nested_attributes_for :covers, allow_destroy: true
 
-  def as_json(_opts = {})
-    {
-      id: id,
-      title: title,
-      description: description,
-      errors: errors,
-      picture_photos: pictures.map do |x| 
-        {
-          url: x.photo.url.absolute_url,
-          name: x.photo_file_name,
-          id: x.id
-        }
-      end
-    }
-  end
+  # def as_json(_opts = {})
+  #   {
+  #     id: id,
+  #     title: title,
+  #     description: description,
+  #     errors: errors,
+  #     picture_photos: pictures.map do |x| 
+  #       {
+  #         url: x.photo.url.absolute_url,
+  #         name: x.photo_file_name,
+  #         id: x.id
+  #       }
+  #     end
+  #   }
+  # end
 end
